d8b9c93edf9068a0c186436430390146a0b90e93
Elaquivala <bombertime4motion@gmail.com>
2023-11-30 12:16:08 +0100

    Add user creation functionality

--- src/Controller/CreateUserController.php
+++ src/Controller/CreateUserController.php
@@ -2,17 +2,54 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 
 class CreateUserController extends AbstractController
 {
-    #[Route('/create/user', name: 'app_create_user')]
-    public function index(): Response
+    #[Route('/user', name: 'app_user')]
+    public function createUser(Request $request, EntityManagerInterface $entityManager, SluggerInterface $slugger): Response
     {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $photoFile = $form->get('Photo')->getData();
+
+            if ($photoFile) {
+                $originalFilename = pathinfo($photoFile->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-'.$user->getId().'.'.$photoFile->guessExtension();
+
+                try {
+                    $photoFile->move(
+                        $this->getParameter('photo_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    // handle exception if something happens during file upload
+                }
+
+                $user->setPhoto($newFilename);
+            }
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('home');
+        }
+
         return $this->render('create_user/index.html.twig', [
-            'controller_name' => 'CreateUserController',
+            'form' => $form->createView(),
         ]);
     }
+
 }
--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -1,18 +1,34 @@
 <?php
 
+// IndexController.php
+
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+
 
 class IndexController extends AbstractController
 {
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     #[Route('/index', name: 'app_index')]
     public function index(): Response
     {
+        $userRepository = $this->entityManager->getRepository(User::class);
+        $users = $userRepository->findAll();
+
         return $this->render('index/index.html.twig', [
-            'controller_name' => 'IndexController',
+            'users' => $users,
         ]);
     }
 }
+
